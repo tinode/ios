b6f43286d620a87f8f800f9d137ddd120bbdec5f
or-else <or.else@gmail.com>
2019-04-17 18:17:02 +0300

    make onFailure handler optional, remove redundant breaks

--- Tinodios/LoginViewController.swift
+++ Tinodios/LoginViewController.swift
@@ -18,6 +18,10 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        // Listen to text change events
+        userNameTextEdit.addTarget(self, action: #selector(textFieldDidChange(_:)), for: UIControl.Event.editingChanged)
+        passwordTextEdit.addTarget(self, action: #selector(textFieldDidChange(_:)), for: UIControl.Event.editingChanged)
     }
     
     private func routeToChats() {
@@ -26,35 +30,56 @@ class LoginViewController: UIViewController {
         
         self.show(destinationVC, sender: nil)
     }
-    
+
+    @objc func textFieldDidChange(_ textField: UITextField) {
+        // Reset red border to default.
+        textField.layer.borderWidth = 0.0
+    }
+
     @IBAction func loginClicked(_ sender: Any) {
-        if let userName = userNameTextEdit.text, let password = passwordTextEdit.text {
-            let tinode = Cache.getTinode()
-            // TODO: implement TLS.
-            do {
-                try tinode.connect(to: Cache.kHostName, useTLS: false)?
-                    .then(
-                        onSuccess: { pkt in
-                            return try tinode.loginBasic(uname: userName, password: password)
-                        }, onFailure: nil)?
-                    .then(
-                        onSuccess: { [weak self] pkt in
-                            print("login successful for: \(tinode.myUid!)")
-                            if let token = tinode.authToken, !token.isEmpty {
-                                let tokenSaveSuccessful = KeychainWrapper.standard.set(
-                                    token, forKey: LoginViewController.kTokenKey)
-                                if !tokenSaveSuccessful {
-                                    print("Could not save auth token...")
-                                }
+        let userName = isTextFieldValid(userNameTextEdit)
+        let password = isTextFieldValid(passwordTextEdit)
+
+        if (userName == "" || password == "") {
+            print("form elements are empty")
+            return
+        }
+
+        let tinode = Cache.getTinode()
+        // TODO: implement TLS.
+        do {
+            try tinode.connect(to: Cache.kHostName, useTLS: false)?
+                .then(
+                    onSuccess: { pkt in
+                        return try tinode.loginBasic(uname: userName, password: password)
+                    })?
+                .then(
+                    onSuccess: { [weak self] pkt in
+                        print("login successful for: \(tinode.myUid!)")
+                        if let token = tinode.authToken, !token.isEmpty {
+                            let tokenSaveSuccessful = KeychainWrapper.standard.set(
+                                token, forKey: LoginViewController.kTokenKey)
+                            if !tokenSaveSuccessful {
+                                print("Could not save auth token...")
                             }
-                            self?.routeToChats()
-                            return nil
-                        }, onFailure: nil)
-            } catch {
-                //os_log("Failed to connect/login to Tinode: %s.", log: OSLog.default, type: .error, error as CVarArg)
-                print("Failed to connect/login to Tinode: \(error).")
-            }
+                        }
+                        self?.routeToChats()
+                        return nil
+                    })
+        } catch {
+            //os_log("Failed to connect/login to Tinode: %s.", log: OSLog.default, type: .error, error as CVarArg)
+            print("Failed to connect/login to Tinode: \(error).")
         }
     }
-    
+
+    private func isTextFieldValid(_ field: UITextField) -> String {
+        let text = (field.text ?? "").trimmingCharacters(in: .whitespacesAndNewlines)
+        if (text.isEmpty) {
+            // Make border red to signify error.
+            field.layer.borderColor = UIColor.red.cgColor
+            field.layer.borderWidth = 1.0
+            return ""
+        }
+        return text
+    }
 }
