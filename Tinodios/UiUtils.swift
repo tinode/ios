0a271372767a695eadfe7f0151b34e2251097a4a
or-else <or.else@gmail.com>
2019-04-20 20:00:55 +0300

    add android-style toast

--- Tinodios/UiUtils.swift
+++ Tinodios/UiUtils.swift
@@ -66,6 +66,27 @@ class UiUtils {
     }
 }
 
+extension UIViewController {
+    // Displays Android-style toast
+    func showToast(message : String, duration: TimeInterval = 3.0) {
+        let toastLabel = UILabel(frame: CGRect(x: self.view.frame.size.width/2 - 75, y: self.view.frame.size.height-100, width: 150, height: 35))
+        toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+        toastLabel.textColor = UIColor.white
+        toastLabel.textAlignment = .center;
+        toastLabel.font = UIFont.preferredFont(forTextStyle: .footnote)
+        toastLabel.text = message
+        toastLabel.alpha = 1.0
+        toastLabel.layer.cornerRadius = 10;
+        toastLabel.clipsToBounds  =  true
+        self.view.addSubview(toastLabel)
+        UIView.animate(withDuration: duration, delay: 0.1, options: .curveEaseOut, animations: {
+            toastLabel.alpha = 0.0
+        }, completion: {(isCompleted) in
+            toastLabel.removeFromSuperview()
+        })
+    }
+}
+
 extension UIImage {
 
     // Resize image to given dimentions. If 'clip' is true and aspect ratios are different, crop the central
