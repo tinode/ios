35d1d5a669770d4dfa89a23085e2179f2ae9b710
or-else <or.else@gmail.com>
2019-05-09 19:23:56 +0300

    placeholder text is correctly shown now

---
+++ Tinodios/widgets/PlaceholderTextView.swift
@@ -0,0 +1,83 @@
+//
+//  PlaceholderTextView.swift
+//  Tinodios
+//
+//  Created by Gene Sokolov on 09/05/2019.
+//  Copyright © 2019 Tinode. All rights reserved.
+//
+
+import UIKit
+
+// UITexView with an optional placeholder text.
+@IBDesignable class PlaceholderTextView: UITextView {
+
+    private enum Constants {
+        static let defaultPlaceholderColor = UIColor(red: 0.0, green: 0.0, blue: 25/255, alpha: 0.22)
+        static let defaultPlaceholderText = "AbCd..."
+    }
+
+    private var isShowingPlaceholder: Bool = false
+
+    @IBInspectable var placeholderText: String = Constants.defaultPlaceholderText
+
+    @IBInspectable open var mainTextColor: UIColor = UIColor.black
+    @IBInspectable open var placeholderColor: UIColor = Constants.defaultPlaceholderColor
+
+    override var text: String! {
+        didSet {
+            checkForEmptyText()
+        }
+    }
+
+    override var attributedText: NSAttributedString! {
+        didSet {
+            checkForEmptyText()
+        }
+    }
+
+    override public init(frame: CGRect, textContainer: NSTextContainer?) {
+        super.init(frame: frame, textContainer: textContainer)
+        addTextChangeObserver()
+        if text.isEmpty {
+            textColor = placeholderColor
+            text = placeholderText
+            isShowingPlaceholder = true
+        }
+    }
+
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        addTextChangeObserver()
+        if text.isEmpty {
+            isShowingPlaceholder = true
+            textColor = placeholderColor
+            text = placeholderText
+        }
+    }
+
+    private func addTextChangeObserver() {
+        NotificationCenter.default.addObserver(self, selector: #selector(textBeginEditing), name: UITextView.textDidBeginEditingNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(checkForEmptyText), name: UITextView.textDidEndEditingNotification, object: nil)
+    }
+
+    @objc private func textBeginEditing() {
+        if isShowingPlaceholder {
+            text = nil
+            textColor = mainTextColor
+            isShowingPlaceholder = false
+        }
+    }
+
+    @objc private func checkForEmptyText() {
+        if text.isEmpty && !isShowingPlaceholder {
+            text = placeholderText
+            textColor = placeholderColor
+            isShowingPlaceholder = true
+        }
+    }
+
+    deinit {
+        NotificationCenter.default.removeObserver(self, name: UITextView.textDidBeginEditingNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UITextView.textDidEndEditingNotification, object: nil)
+    }
+}
