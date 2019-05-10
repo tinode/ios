352f1028d5a588ce161c69243ff55077f2d6c8f0
aforge <aforge@>
2019-05-10 01:53:23 -0700

    Load the list of contacts from Users table in BaseDb.

--- Tinodios/FindRouter.swift
+++ Tinodios/FindRouter.swift
@@ -17,10 +17,10 @@ class FindRouter: FindRoutingLogic {
     
     func routeToChat(segue: UIStoryboardSegue) {
         if let indexPath = viewController?.tableView.indexPathForSelectedRow,
-            let ims = viewController?.contacts[indexPath.row].ims, !ims.isEmpty {
+            let uniqueId = viewController?.contacts[indexPath.row].uniqueId, !uniqueId.isEmpty {
             // TODO: replace Find VC instead of pushing it on the stack.
             let messageController = segue.destination as! MessageViewController
-            messageController.topicName = ims[0]
+            messageController.topicName = uniqueId
         }
     }
 }
