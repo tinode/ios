8ab2959dac529a6c96c4de5a0cce4f4f6ca72fc5
or-else <or.else@gmail.com>
2019-05-22 19:42:11 +0300

    cold start issue, show found matches

--- Tinodios/FindInteractor.swift
+++ Tinodios/FindInteractor.swift
@@ -73,7 +73,7 @@ class FindInteractor: FindBusinessLogic {
     func updateAndPresentRemoteContacts() {
         if let subs = fndTopic?.getSubscriptions() {
             self.remoteContacts = subs.map { sub in
-                return ContactHolder(displayName: sub.pub?.fn, image: sub.pub?.photo?.image(), uniqueId: sub.uniqueId)
+                return ContactHolder(displayName: sub.pub?.fn, image: sub.pub?.photo?.image(), uniqueId: sub.uniqueId, subtitle: sub.priv?.joined(separator: ", "))
             }
         } else {
             self.remoteContacts?.removeAll()
