053103b0a8ab6a5fc7d888f6ca33316cd077cad8
or-else <or.else@gmail.com>
2019-09-10 12:05:24 +0300

    adding muted + blocked icons to contacts

--- Tinodios/widgets/ChatListViewCell.swift
+++ Tinodios/widgets/ChatListViewCell.swift
@@ -9,16 +9,22 @@ import UIKit
 import TinodeSDK
 
 class ChatListViewCell: UITableViewCell {
+    private static let kIconWidth: CGFloat = 18
+    private static let kIconSeparator: CGFloat = 4
 
     @IBOutlet weak var icon: AvatarWithOnlineIndicator!
     @IBOutlet weak var title: UILabel!
     @IBOutlet weak var subtitle: UILabel!
     @IBOutlet weak var unreadCount: UILabel!
-    @IBOutlet weak var blockedIcon: UIImageView!
-    @IBOutlet weak var mutedIcon: UIImageView!
+    @IBOutlet weak var iconBlocked: UIImageView!
+    @IBOutlet weak var iconMuted: UIImageView!
+    @IBOutlet weak var iconBlockedWidth: NSLayoutConstraint!
+    @IBOutlet weak var unreadCountWidth: NSLayoutConstraint!
 
     override func awakeFromNib() {
         super.awakeFromNib()
+        iconMuted.tintColor = UIColor.init(fromHexCode: 0xFFCCCCCC)
+        iconBlocked.tintColor = iconMuted.tintColor
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -33,9 +39,16 @@ class ChatListViewCell: UITableViewCell {
         if unread > 0 {
             unreadCount.text = unread > 9 ? "9+" : String(unread)
             unreadCount.isHidden = false
+            unreadCountWidth.constant = ChatListViewCell.kIconWidth
         } else {
             unreadCount.isHidden = true
+            unreadCountWidth.constant = .leastNonzeroMagnitude
         }
+
+        iconBlocked.isHidden = !topic.isJoiner
+        iconBlockedWidth.constant = topic.isJoiner ? .leastNonzeroMagnitude : ChatListViewCell.kIconWidth + ChatListViewCell.kIconSeparator * 2
+
+        iconMuted.isHidden = !topic.isMuted
 
         // Avatar image
         icon.set(icon: topic.pub?.photo?.image(), title: topic.pub?.fn, id: topic.name, online: topic.online)
