639a0b1aea3a9d7adef38ffd58c0658a3a63ccf7
or-else <or.else@gmail.com>
2019-10-21 14:33:33 +0300

    use 1.0 for marketing version. otherwise Apple rejects the submission

--- Scripts/set_build_number.sh
+++ Scripts/set_build_number.sh
@@ -22,7 +22,7 @@ dsym_plist="$DWARF_DSYM_FOLDER_PATH/$DWARF_DSYM_FILE_NAME/Contents/Info.plist"
 for plist in "$target_plist" "$dsym_plist"; do
   if [ -f "$plist" ]; then
     /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $number_of_commits" "$plist"
-    # Disabled for now. Apple wants versions like 1.2.3. Can't use it just yet.
+    # Disabled for now. Apple wants versions like 1.2.3. Can't use tag name just yet.
     # /usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString ${git_release_version#*v}" "$plist"
   fi
 done
