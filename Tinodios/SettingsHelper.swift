aab2954a5ce09859c576389ca173a387362e8862
or-else <or.else@gmail.com>
2019-10-22 21:49:19 +0300

    initial connection preferences are now set correctly

--- Tinodios/SettingsHelper.swift
+++ Tinodios/SettingsHelper.swift
@@ -16,9 +16,17 @@ class SettingsHelper {
     }
 
     class func getConnectionSettings() -> (hostName: String?, useTLS: Bool?, wireTransport: String?) {
-        return (hostName: UserDefaults.standard.string(forKey: SettingsBundleKeys.hostName),
-                useTLS: UserDefaults.standard.bool(forKey: SettingsBundleKeys.useTLS),
-                wireTransport: UserDefaults.standard.string(forKey: SettingsBundleKeys.wireTransport))
+        return (hostName: UserDefaults.standard.string(forKey: SettingsBundleKeys.hostName), useTLS: UserDefaults.standard.bool(forKey: SettingsBundleKeys.useTLS), wireTransport: UserDefaults.standard.string(forKey: SettingsBundleKeys.wireTransport))
+    }
+
+    class func setHostName(_ hostName: String?) {
+        guard hostName != nil else { return }
+        UserDefaults.standard.set(hostName, forKey: SettingsBundleKeys.hostName)
+    }
+
+    class func setUseTLS(_ useTLS: String?) {
+        guard let useTLS = useTLS else { return }
+        UserDefaults.standard.set(NSString(string: useTLS).boolValue, forKey: SettingsBundleKeys.useTLS)
     }
 }
 
