b35fd2c99591848ecd7d1f87aed0b7a37a8c18ad
or-else <or.else@gmail.com>
2020-05-11 16:07:02 +0300

    missing file

---
+++ Tinodios/SettingsGeneralViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SettingsGeneralViewController.swift
+//  Tinodios
+//
+//  Copyright © 2020 Tinode. All rights reserved.
+//
+
+import UIKit
+
+class SettingsPersonalViewController: UITableViewController {
+}
+
