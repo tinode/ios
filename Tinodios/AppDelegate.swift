2b3ab7bacca4034e038d853f8b60d16a6dfc74b1
or-else <or.else@gmail.com>
2020-06-18 10:23:56 +0300

    removed unnecessary initialization

--- Tinodios/AppDelegate.swift
+++ Tinodios/AppDelegate.swift
@@ -23,7 +23,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var appIsStarting: Bool = false
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        FirebaseApp.configure()
         Utils.registerUserDefaults()
         let baseDb = BaseDb.getInstance()
         if baseDb.isReady {
