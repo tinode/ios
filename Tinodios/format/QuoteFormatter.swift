4de4d1c92d57bd4c5d5591499e7bd5ea86a71675
or-else <or.else@gmail.com>
2022-06-05 11:39:58 -0700

    bug with double localization

--- Tinodios/format/QuoteFormatter.swift
+++ Tinodios/format/QuoteFormatter.swift
@@ -66,7 +66,7 @@ class QuoteFormatter: PreviewFormatter {
         } else {
             annotation = NSLocalizedString("Attachment", comment: "Label shown next to an attachment")
         }
-        return PreviewFormatter.annotatedIcon(iconName: "paperclip", annotation: annotation, comment: "Attachment preview icon.")
+        return PreviewFormatter.annotatedIcon(iconName: "paperclip", localizedAnnotation: annotation)
     }
 
     override func handleMention(content nodes: [FormatNode], using data: [String: JSONValue]?) -> FormatNode {
