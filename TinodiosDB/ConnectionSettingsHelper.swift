67af4aa071300bd2943578af0ea84cb513870a8c
or-else <or.else@gmail.com>
2025-05-29 15:49:41 +0300

    fix app settings by removing unused wire transport section from Settings.bundle

--- TinodiosDB/ConnectionSettingsHelper.swift
+++ TinodiosDB/ConnectionSettingsHelper.swift
@@ -2,7 +2,7 @@
 //  SettingsHelper.swift
 //  TinodiosDB
 //
-//  Copyright © 2019 Tinode. All rights reserved.
+//  Copyright © 2019-2025 Tinode. All rights reserved.
 //
 
 import Foundation
@@ -12,11 +12,10 @@ public class ConnectionSettingsHelper {
     struct SettingsBundleKeys {
         static let hostName = "host_name_preference"
         static let useTLS = "use_tls_preference"
-        static let wireTransport = "wire_transport_preference"
     }
 
-    class func getConnectionSettings() -> (hostName: String?, useTLS: Bool?, wireTransport: String?) {
-        return (hostName: SharedUtils.kAppDefaults.string(forKey: SettingsBundleKeys.hostName), useTLS: SharedUtils.kAppDefaults.bool(forKey: SettingsBundleKeys.useTLS), wireTransport: SharedUtils.kAppDefaults.string(forKey: SettingsBundleKeys.wireTransport))
+    class func getConnectionSettings() -> (hostName: String?, useTLS: Bool?) {
+        return (hostName: SharedUtils.kAppDefaults.string(forKey: SettingsBundleKeys.hostName), useTLS: SharedUtils.kAppDefaults.bool(forKey: SettingsBundleKeys.useTLS))
     }
 
     class func setHostName(_ hostName: String?) {
